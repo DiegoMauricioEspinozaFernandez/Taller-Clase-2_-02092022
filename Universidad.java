6f105aa8951199b75c0adb8d5f59b7bcac6680b5
DiegoMauricioEspinozaFernandez <100706192+DiegoMauricioEspinozaFernandez@users.noreply.github.com>
2022-09-27 10:33:36 -0500

    Add files via upload

---
+++ Universidad.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicioclase2;
+
+/**
+ *
+ * @author usuario
+ */
+public class Universidad {
+     public static void main(String[] args) {
+     
+         Persona p=new Persona();
+         
+     
+     
+     }
+}
